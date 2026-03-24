e219f496f8b2bad8c6e17ddfefa51f88e416f446
Hpaliwal99 <hpaliwal99@gmail.com>
2026-03-24 18:37:43 +0000

    Updated files

---
+++ src/main/java/ATLAS/NodeStack.java
@@ -0,0 +1,10 @@
+package ATLAS;
+
+import java.util.*;
+
+public class NodeStack {
+    public NodeStack(String input) {
+        Queue<String> q = new LinkedList<>();
+
+    }
+}
--- src/test/java/MainSuite.java
+++ src/test/java/MainSuite.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 
 public class MainSuite {
+
+    @Test
+    public void maintest() {
+        int x = new Random().nextInt(5);
+        assertEquals(3, x);
+    }
 }
