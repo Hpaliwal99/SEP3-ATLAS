57ff4732c0913367b090cdbc6c4bb9a32ef33ae2
P0u5i <Pouriakhv2@gmail.com>
2026-03-30 19:26:41 +0100

    made String compare

--- src/main/java/ATLAS/Utility.java
+++ src/main/java/ATLAS/Utility.java
@@ -5,54 +5,56 @@ import java.util.*;
 
 public class Utility {
 
-    public Boolean StringCompare(String A, String B) throws ParseException {
-        int index = 0;
+    public static Boolean StringCompare(String A, String B) throws ParseException {
         String[] tokensA = A.replace(")", "").split("\\(");
         tokensA = Arrays.copyOfRange(tokensA, 1, tokensA.length);
         if(tokensA.length == 0){
             throw new ParseException("No tokens found after stripping parentheses.", 0);
         }
-        System.out.println(Arrays.toString(tokens)); // Debug Line
+
+        String[] tokensB = B.replace(")", "").split("\\(");
+        tokensB = Arrays.copyOfRange(tokensB, 1, tokensB.length);
+        if(tokensB.length == 0){
+            throw new ParseException("No tokens found after stripping parentheses.", 0);
+        }
+
+        //check depth is the same
+        if(tokensA.length != tokensB.length){
+            return false;
+        }
         String predicate = "";
         String keyword = "";
 
-        Node current = null;
-
         // Check equal length of tokens Array
+        for(int i = 0; i < tokensA.length; i++){
+            String[] tokenListA = tokensA[i].split(" ");
+            String[] tokenListB = tokensB[i].split(" ");
 
-        for (String token : tokens) {
-            String[] tokenList = token.split(" ");
-            if (tokenList.length >= 2){
-                keyword = tokenList[tokenList.length - 1];
-                predicate = String.join(" ", Arrays.copyOfRange(tokenList, 0, tokenList.length-1));
-//                System.out.println(predicate); // Debug Line
-//                predicates.add(predicate);
-//                keywords.add(keyword);
+            if(tokenListA.length != tokenListB.length){
+                return false;
             }
-            else if (tokenList.length == 1){
-                predicate = tokenList[0];
-                keyword = "";
-//                keywords.add(keyword);
-//                predicates.add(predicate);
+            if(tokenListA.length == 1 ){
+                if(!tokenListA[0].equals(tokenListB[0])){
+                    return false;
+                }
             }
-            if (index == 0) {
-//                this.head = new Node(predicate, keyword, index);
-//                current = this.head;
-                index++;
-            } else {
-//                current.children = new Node(predicate, keyword, index);
-//                current = current.children;
-                index++;
-
+            else if(tokenListA.length >= 2){
+                for(int j = 0; j < tokenListA.length - 1; j++){
+                    if(!tokenListA[j].equals(tokenListB[j])){
+                        return false;
+                    }
+                }
+                if(tokenListA[tokenListA.length-1].charAt(0) == '*' ^ tokenListB[tokenListB.length-1].charAt(0) == '*'){
+                    return false;
+                }
             }
         }
-        if(this.head == null){
-            throw new ParseException("parsing produced nothing", 0);
-        }
+        return true;
 
-        return this.head;
+
     }
-
 }
+
+
 
 
