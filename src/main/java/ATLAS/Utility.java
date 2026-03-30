4360737a80598ba292a3a4dea975867de999c967
P0u5i <Pouriakhv2@gmail.com>
2026-03-30 20:13:52 +0100

    made tokenizer and refactored StringCompare

--- src/main/java/ATLAS/Utility.java
+++ src/main/java/ATLAS/Utility.java
@@ -5,54 +5,87 @@ import java.util.*;
 
 public class Utility {
 
-    public static Boolean StringCompare(String A, String B) throws ParseException {
-        String[] tokensA = A.replace(")", "").split("\\(");
+    public static LinkedList<String[]> tokenizer(String a) throws ParseException {
+        LinkedList<String[]> tokens = new LinkedList<>();
+        String[] tokensA = a.replace(")", "").split("\\(");
         tokensA = Arrays.copyOfRange(tokensA, 1, tokensA.length);
         if(tokensA.length == 0){
             throw new ParseException("No tokens found after stripping parentheses.", 0);
         }
-
-        String[] tokensB = B.replace(")", "").split("\\(");
-        tokensB = Arrays.copyOfRange(tokensB, 1, tokensB.length);
-        if(tokensB.length == 0){
-            throw new ParseException("No tokens found after stripping parentheses.", 0);
+        for(int i = 0; i < tokensA.length; i++){
+            tokens.add(tokensA[i].split(" "));
         }
+        return tokens;
+    }
+    public static Boolean StringCompare(String A, String B) throws ParseException {
+        LinkedList<String[]> tokensA = Utility.tokenizer(A);
+        LinkedList<String[]> tokensB = Utility.tokenizer(B);
 
         //check depth is the same
-        if(tokensA.length != tokensB.length){
+        if(tokensA.size() != tokensB.size()){
             return false;
         }
         String predicate = "";
         String keyword = "";
 
         // Check equal length of tokens Array
-        for(int i = 0; i < tokensA.length; i++){
-            String[] tokenListA = tokensA[i].split(" ");
-            String[] tokenListB = tokensB[i].split(" ");
+        for(int i = 0; i < tokensA.size(); i++){
+            String[] TA = tokensA.get(i);
+            String[] TB = tokensB.get(i);
 
-            if(tokenListA.length != tokenListB.length){
+            if(TA.length != TB.length){
                 return false;
             }
-            if(tokenListA.length == 1 ){
-                if(!tokenListA[0].equals(tokenListB[0])){
+            if(TA.length == 1 ){
+                if(!TA[0].equals(TB[0])){
                     return false;
                 }
             }
-            else if(tokenListA.length >= 2){
-                for(int j = 0; j < tokenListA.length - 1; j++){
-                    if(!tokenListA[j].equals(tokenListB[j])){
+            else if(TA.length >= 2){
+                for(int j = 0; j < TA.length - 1; j++){
+                    if(!TA[j].equals(TB[j])){
                         return false;
                     }
                 }
-                if(tokenListA[tokenListA.length-1].charAt(0) == '*' ^ tokenListB[tokenListB.length-1].charAt(0) == '*'){
+                if(TA[TA.length-1].charAt(0) == '*' ^ TB[TB.length-1].charAt(0) == '*'){
                     return false;
                 }
+
             }
         }
         return true;
 
 
     }
+
+
+//    public static LinkedList<String[]> getStringKeywordMapping(String p1, String p2) throws ParseException {
+//
+//    }
+//
+//    public static LinkedList<String[]> getStringKeywordMapping(String p1, String p2) throws ParseException {
+//        LinkedList<String[]> result = new LinkedList<>();
+//
+////        if(p1.getHead() == null || p2.getHead() == null){
+////            throw new ParseException("Nodes can't be null", ((p1.getHead() == null) ? 1 : 2));
+////        }
+//
+//        if (Utility.StringCompare(p1, p2)) {
+//            String[] p1keywords = p1.getKeywords().toArray(new String[0]);
+//            int i = 0;
+//            for (String key : p2.getKeywords()) {
+//                result.add(new String[] {key, p1keywords[i++]});
+//            }
+//        } else {
+//            System.out.println("Structure not equal");
+//            return result;
+//        }
+//
+//        return result;
+//
+//    }
+
+
     public static Boolean compare(Parse p1, Parse p2) throws ParseException {
 
         if(p1.getHead() == null || p2.getHead() == null){
