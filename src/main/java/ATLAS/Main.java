b4fdade607a18e50d0768662cb7105763ab5d18c
Hpaliwal99 <hpaliwal99@gmail.com>
2026-04-10 13:38:20 +0100

    Main test run for importing rules.

--- src/main/java/ATLAS/Main.java
+++ src/main/java/ATLAS/Main.java
@@ -2,42 +2,53 @@ package ATLAS;
 
 
 import java.text.ParseException;
-import java.util.Arrays;
-import java.util.LinkedList;
+import java.util.*;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) throws ParseException {
+    public static void main(String[] args) throws Exception {
+//
+////        String input1 = "(work in *scientist (some lab (that (conduct experiment))))";
+//        String input1 = "(Some artist (create art (with paint)))";
+////        String input2 = "(work in *priest (some church (that (conduct sermon))))";
+//        String input2 = "(Some programmer (create code (with computer)))";
+//        Parse parse1 = new Parse();
+//        Parse parse2 = new Parse();
+//        Node root = parse1.parse(input1);
+//        Node root2 = parse2.parse(input2);
+//
+//        System.out.println();
+//        System.out.println("Printing Parse:");
+//        System.out.println(parse1.printIndent(root));
+//        System.out.println();
+//        System.out.println("Printing flattened Parse:");
+//        System.out.println(parse1.toFlat(root));
+//        System.out.println();
+//        parse1.printQueues();
+//        System.out.println();
+//
+//        parse1.replaceKeywords(root);
+//        System.out.println(parse1.toFlat(root));
+//        System.out.println();
+//        System.out.println(parse1.printIndent(root));
+//
+//        System.out.println(Utility.compare(parse1, parse2));
+//        System.out.println(Utility.toStringLL(Utility.getKeywordMapping(parse1, parse2))); //make custom toString
+//        System.out.println(Utility.StringCompare(input1, input2));
+//        System.out.println(Utility.toStringLL(Utility.getStringKeywordMapping(input1,input2)));
+        RulesUtil rulesUtil = new RulesUtil();
+        rulesUtil.loadRules("src/main/java/ATLAS/rewrite rules.txt");
 
-//        String input1 = "(work in *scientist (some lab (that (conduct experiment))))";
-        String input1 = "(Some artist (create art (with paint)))";
-//        String input2 = "(work in *priest (some church (that (conduct sermon))))";
-        String input2 = "(Some programmer (create code (with computer)))";
-        Parse parse1 = new Parse();
-        Parse parse2 = new Parse();
-        Node root = parse1.parse(input1);
-        Node root2 = parse2.parse(input2);
+        Map<String, List<Rule>> ruleMap =  rulesUtil.getRuleMap();
 
-        System.out.println();
-        System.out.println("Printing Parse:");
-        System.out.println(parse1.printIndent(root));
-        System.out.println();
-        System.out.println("Printing flattened Parse:");
-        System.out.println(parse1.toFlat(root));
-        System.out.println();
-        parse1.printQueues();
-        System.out.println();
+        int i = 0;
+        for (Map.Entry<String, List<Rule>> entry : ruleMap.entrySet()) {
+            if (i > 5) break;
+            System.out.println(entry);
+            i++;
 
-        parse1.replaceKeywords(root);
-        System.out.println(parse1.toFlat(root));
-        System.out.println();
-        System.out.println(parse1.printIndent(root));
-
-        System.out.println(Utility.compare(parse1, parse2));
-        System.out.println(Utility.toStringLL(Utility.getKeywordMapping(parse1, parse2))); //make custom toString
-        System.out.println(Utility.StringCompare(input1, input2));
-        System.out.println(Utility.toStringLL(Utility.getStringKeywordMapping(input1,input2)));
+        }
 
     }
 }
